56008685c9b9f53ee109759fbbd2932ac65efb57
Abdulbaki <abdoladepo@gmail.com>
2026-02-28 18:10:40 +0100

    trigger redeploy for preview image

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import QuizSetup from './components/QuizSetup'
 import QuizPage from './components/QuizPage'
 import Results from './components/Results'
@@ -7,13 +8,16 @@ import About from './components/About'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<QuizSetup />} />
-      <Route path="/quiz" element={<QuizPage />} />
-      <Route path="/results" element={<Results />} />
-      <Route path="/history" element={<QuizHistory />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<QuizSetup />} />
+        <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/history" element={<QuizHistory />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   )
 }
 
